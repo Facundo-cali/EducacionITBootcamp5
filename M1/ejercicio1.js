e7e97057bb0164efa84ecc04b9a784a3757bdd16
Facundo-cali <facundocali1234@gmail.com>
2023-06-05 22:02:48 -0300

    M1 DESAFIO 2 COMPLETO

--- M1/ejercicio1.js
+++ M1/ejercicio1.js
@@ -1,14 +1,15 @@
 const button = document.querySelector("button");
-var formulario = document.getElementById('form');
+var form = document.getElementById('form');
 
-formulario.addEventListener('submit', function(event) {
+
+form.addEventListener('submit', function(event) {
     event.preventDefault();
     var costosFijos = document.getElementById('costos-fijos').value;
     var precioVenta = document.getElementById('precio-venta').value;
     var costosVariables = document.getElementById('costos-variables').value;   
-    alert("El punto de equilibrio es: " + (costosFijos / (precioVenta - costosVariables)));
+    var resultado = costosFijos / (precioVenta - costosVariables);
+    document.getElementById('resultado').textContent = "El punto de equilibrio es: " + resultado;
 });
-
 
 
 
